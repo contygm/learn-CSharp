e44f59645d8229e9b839ee889656c7fcfa879591
Genevieve Conty <gmireillec@Genevieves-MacBook-Pro.local>
2017-02-27 22:13:01 -0500

    finished StudentInfo2

--- Chapter1-Basics/3-ArrayListsDicts/StudentInfo2.cs
+++ Chapter1-Basics/3-ArrayListsDicts/StudentInfo2.cs
@@ -7,7 +7,6 @@
  *
  *  PART 2
  *  prompts for additional students
- *   
  *     1. Easy 
  *			- user enters values again
  *			- print the information after confirmation
@@ -33,42 +32,65 @@ class Program
 {
 	static void Main(string[] args)
 	{
-
+		// Master List
+		List<string> pupils = new List<string>();
+		
 		while(true)
 		{
+			// Student info
+			Dictionary<string, string> student = new Dictionary<string, string>();
+
+			//gather info
 			Console.WriteLine("Please enter your first name.");
 			string firstName = System.Console.ReadLine();
+			student.Add("First Name: ", firstName);
 
-			Console.WriteLine("Please enter your middle initial.");
-			string middleInitial = System.Console.ReadLine();
+			Console.WriteLine("Please enter your middle name.");
+			string middleName = System.Console.ReadLine();
+			student.Add("Middle Name:", middleName);
 
 			Console.WriteLine("Please enter your last name.");
 			string lastName = System.Console.ReadLine();
+			student.Add("Last Name: ", lastName);
 
 			Console.WriteLine("Please enter your address.");
 			string address = System.Console.ReadLine();
+			student.Add("Address:", address);
 
 			Console.WriteLine("Please enter your phone number.");
 			string phone = System.Console.ReadLine();
+			student.Add("Phone Number:", phone);
 
 			Console.WriteLine("Please enter your email.");
 			string email = System.Console.ReadLine();
+			student.Add("Email: ", email);
 
-			Console.WriteLine("Is your information correct? [Enter 'Y' for yes.]");
+			// confirm correct student info
+			Console.WriteLine("Is the information correct? [Enter 'Y' for yes.]");
 			var confirmation = System.Console.ReadLine();
 
 			if (confirmation == "Y"){
-				Console.WriteLine("Your full name is "+ firstName +" "+ middleInitial +" "+ lastName + ".");
-				Console.WriteLine("Your address is "+ address +".");
-				Console.WriteLine("Your phone number is "+ phone +".");
-				Console.WriteLine("Your email is  "+ email +".");
-				break;
-			}
-				
+				// show most recent addition
+				foreach (KeyValuePair<string, string> entry in student)
+			    {
+			      Console.WriteLine("Added Student: ");
+			      Console.WriteLine(entry.Key + " : " + entry.Value);
+			      pupils.Add(entry.Value);
+			    }
+
+			    // prompt for adding another student
+			    Console.WriteLine("Do you want to add another student? [Enter 'Y' for yes.]");
+			    var addAnother = System.Console.ReadLine();
+			    
+			    // print master list when done adding
+			    if (addAnother != "Y"){
+					foreach (string pupil in pupils)
+				    {
+				        Console.WriteLine(pupil);
+				    }
+					break;
+				}
+			}		
 		}
-		
-
-
 	}
-	
 }
